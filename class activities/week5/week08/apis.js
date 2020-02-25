3ddf54f7925bc926c164182bf6fc3b9a637ddb78
chelblasz <chelseablaszczyk@gmial.com>
2020-02-25 14:21:32 -0700

    pokimon

---
+++ class activities/week5/week08/apis.js
@@ -0,0 +1,34 @@
+let data;
+const previous = document.querySelector(".previous");
+const next = document.querySelector(".next");
+
+// previous.addEventListener("click", )
+
+
+function pokiFetch(url) {
+    return fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+
+}
+
+function pokiDisplay(listed) {
+    const list = document.querySelector("ul");
+    list.innerHTML = '';
+    for (let i = 0; i < listed.length; i++) {
+        list.innerHTML += `<li><a>${listed[i].name}</a></li>`;
+    }
+};
+
+async function buildPage(url = 'https://pokeapi.co/api/v2/pokemon/') {
+    const pokiDeck = await pokiFetch(url);
+    pokiDisplay(pokiDeck.results)
+    console.log(pokiDeck);
+}
+
+buildPage();
+
+next.addEventListener("click", () => {buildPage(pokiFetch(pokiDeck.next))});
+
+
