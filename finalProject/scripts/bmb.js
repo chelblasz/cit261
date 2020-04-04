5a0dc03b3cf20014a9047da46b032493fe716ce1
chelblasz <chelseablaszczyk@gmial.com>
2020-04-03 20:47:58 -0600

    save

--- finalProject/scripts/bmb.js
+++ finalProject/scripts/bmb.js
@@ -1,15 +1,121 @@
-// import {trapezius, booty, obliques, abs } from './exercises.js'
+/*SUDO:
+Trapezius is clicked
+JS is listening and when traps are clicked JS will fetch the JSON
+Json will deliver the exercises associated with the muscle cliked (traps)
+JS will make it appear in a list in HTML where the Section tag is */
+
+let exerciseList = [];
 
 document.querySelector('#bodyBack').addEventListener('click', (e) => {
     e.preventDefault();
-    console.log(e.target);
-    listElements(e.target.title);
+    const type = e.target.title.split('-');
+    // store type in array If this is in the array do not add and if it is not then add.
+    listElements(type[0]);
 });
 
-function listElements() {
-    let section = document.createElement('section');
-    section.appendChild(h2);
-    console.log(e.target);
+function listElements(e) {
+    exerciseList.filter(exercise => exercise.type.toLowerCase() == e).forEach(exercise => {
+        renderEx(exercise);
+
+    });
 }
 
-// 
+function renderEx(modify) {
+    let eList = document.querySelector("#exerciseList");
+    eList.innerHTML += `
+        <div>
+            ${modify.target}
+            <div>${modify.name}</div>
+            <div>${modify.equipment}</div>
+            <div>${modify.description}</div>
+        </div>
+    `;
+}
+
+function getExercises() {
+    fetch('../exercises.json')
+        .then(response => {
+            response.json()
+                .then(
+                    response => {
+                        exerciseList = response.exercises;
+                        
+                    }
+                )
+        });
+}
+
+function deleteList(){
+    document.querySelector('#delete').addEventListener('click', (e) => {
+    document.querySelector('#exerciseList').innerHTML = '';
+    });
+}
+
+getExercises();
+
+
+
+
+
+// exercises.forEach(
+                        //     (exercise) => {
+                        //         if (exercise.type === 'Trapezius') {
+                        //            let eList = document.querySelector("#exerciseList");
+                        //             eList.innerHTML += `
+                        //                 <div>
+                        //                     ${exercise.target}
+                        //                     <div>${exercise.name}</div>
+                        //                     <div>${exercise.equipment}</div>
+                        //                     <div>${exercise.description}</div>
+                        //                 </div>
+                        //                 <br>
+                        //             `;
+
+                        //          } else if (exercise.type === 'Glutes') {
+                        //             let eList = document.querySelector("#exerciseList");
+                        //             eList.innerHTML += `
+                        //                 <div>
+                        //                     ${exercise.target}
+                        //                     <div>${exercise.name}</div>
+                        //                     <div>${exercise.equipment}</div>
+                        //                     <div>${exercise.description}</div>
+                        //                 </div>
+                        //                 <br>
+                        //             `;
+
+                        //             // document.querySelector('.glutes')
+                        //             //     .textContent = exercises.target;
+
+                        //          } else if (exercise.type === 'Obliques') {
+                        //             let eList = document.querySelector("#exerciseList");
+                        //             eList.innerHTML += `
+                        //                 <div>
+                        //                          ${exercise.target}
+                        //                     <div>${exercise.name}</div>
+                        //                     <div>${exercise.equipment}</div>
+                        //                     <div>${exercise.description}</div>
+                        //                 </div>
+                        //                 <br>
+                        //             `;
+
+                        //             // document.querySelector('.obliques')
+                        //             //     .textContent = exercises.target;
+
+                        //          } else if (exercise.type === 'Abs') {
+                        //             let eList = document.querySelector("#exerciseList");
+                        //             eList.innerHTML += `
+                        //                 <div>
+                        //                          ${exercise.target}
+                        //                     <div>${exercise.name}</div>
+                        //                     <div>${exercise.equipment}</div>
+                        //                     <div>${exercise.description}</div>
+                        //                 </div>
+                        //                 <br>
+                        //             `;
+
+                        //             // document.querySelector('.abs')
+                        //             //     .textContent = exercises.target;
+
+                        //          }
+                        // }
+                        // );
